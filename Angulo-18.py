fdbfba2b73129994d8ac577a72d77cdfe005164b
Rykelme <p.rykelmeandrei@gmail.com>
2026-03-01 19:17:55 -0300

    Fix input prompt and output formatting

--- Angulo-18.py
+++ Angulo-18.py
@@ -1,7 +1,7 @@
 import math
 co = float(input('cateto oposto:'))
-ca = float(input('cateto adjasente'))
+ca = float(input('cateto adjacente'))
 h = math.hypot (co,ca)
 rad = math.atan2(co,ca)
 grau = math.degrees(rad)
-print (f'co{co:.2f},ca{ca:.2f},hip{h:.2f},rad{rad:.2f},grau{grau:.2f}')
+print (f'co: {co:.2f} ,ca: {ca:.2f} , hip: {h:.2f} , rad: {rad:.2f} , grau: {grau:.2f}')
